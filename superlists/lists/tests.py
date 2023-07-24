eb2922c62e9a3f322f176f8449b534c477a98d75
Станислав Бескаев <stanislav.beskaev@gmail.com>
2023-07-24 21:35:35 +0300

    с 74, расширен функциональный тест, проверка заголовков, ввод и отправка нового элемента

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -1,6 +1,6 @@
+from django.http import HttpRequest
 from django.test import TestCase
 from django.urls import resolve
-from django.http import HttpRequest
 
 from superlists.lists.views import home_page
 
--- superlists/tests/functional/functional_tests.py
+++ superlists/tests/functional/functional_tests.py
@@ -1,6 +1,9 @@
+import time
 import unittest
 
 from selenium import webdriver
+from selenium.webdriver import Keys
+from selenium.webdriver.common.by import By
 
 APP_URL = "http://localhost:8000"
 
@@ -22,18 +25,31 @@ class NewVisitorTest(unittest.TestCase):
 
         # Она видит, что заголовок и шапка страницы говорят о списках неотложных дел
         self.assertIn("To-Do", self.browser.title)
-        self.fail("Закончить тест!")
+        header_text = self.browser.find_element(by=By.TAG_NAME, value="h1").text
+        self.assertIn("To-Do", header_text)
 
         # Ей сразу же предлагается ввести элемент списка
+        inputbox = self.browser.find_element(by=By.ID, value="idi_new_item")
+        self.assertEqual(inputbox.get_attribute("placeholder"), "Enter a to-do item")
 
         # Она набирает в текстовом поле "Купить павлиньи перья" (ее хобби - вязание рыболовных мушек)
+        inputbox.send_keys("Купить павлиньи перья")
 
         # Когда она нажимает enter, страница обновляется, и теперь страница
         # содержит "1: Купить павлиньи перья" в качестве элемента списка
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)  # Явное ожидание перезагрузки страницы
+
+        table = self.browser.find_element(by=By.ID, value="id_list_table")
+        rows = table.find_elements(by=By.TAG_NAME, value="tr")
+        self.assertTrue(
+            any(row.text == "1: Купить павлиньи перья" for row in rows)
+        )
 
         # Текстовое поле по-прежнему приглашает её добавить ещё один элемент.
         # Она вводит "Сделать мушку из павлиньих перьев"
         # (Эдит очень методична)
+        self.fail("Закончить тест!")
 
         # Страница снова обновляется, и теперь показывает оба элемента её списка
 
