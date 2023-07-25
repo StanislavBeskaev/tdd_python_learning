99eb3e65a6968925565635be50015bd86ba9495d
Станислав Бескаев <stanislav.beskaev@gmail.com>
2023-07-25 11:08:04 +0300

    с 98, в ФТ добавлена проверка наличия второго элемента, модульные тесты проверяют POST запрос, в шаблон добавлена форма и наивный вывод нового элемента в таблице

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -1,10 +1,20 @@
 from django.test import TestCase
 
 
+HOME_TEMPLATE = "home.html"
+
+
 class HomePageTest(TestCase):
     """Тест домашней страницы"""
 
     def test_user_home_template(self):
         """Тест: использование корректного шаблона"""
         response = self.client.get("/")
-        self.assertTemplateUsed(response, "home.html")
+        self.assertTemplateUsed(response, HOME_TEMPLATE)
+
+    def test_can_save_a_POST_request(self):
+        """Тест: можно сохранить post-запрос"""
+        new_item = "A new list item"
+        response = self.client.post("/", data={"item_text": new_item})
+        self.assertIn(new_item, response.content.decode())
+        self.assertTemplateUsed(response, HOME_TEMPLATE)
