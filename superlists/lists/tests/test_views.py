728ccdc5946f2efa6ce81b3086402194fd98a2cf
Станислав Бескаев <stanislav.beskaev@gmail.com>
2023-07-30 14:15:43 +0300

    с 273, get_absolute_url для модели List и убрано жёсткое кодирование url в представлениях

--- superlists/lists/tests/test_views.py
+++ superlists/lists/tests/test_views.py
@@ -113,3 +113,10 @@ class ListViewTest(TestCase):
         self.assertTemplateUsed(response, "list.html")
         expected_error = escape("You can't have an empty list item")
         self.assertContains(response, expected_error)
+
+    def test_invalid_list_items_arent_saved(self):
+        """Тест: не сохраняются недопустимые элементы списка"""
+        list_ = List.objects.create()
+        self.client.post(f"/lists/{list_.id}/", data={"item_text": ""})
+        self.assertEqual(List.objects.count(), 1)
+        self.assertEqual(Item.objects.count(), 0)
