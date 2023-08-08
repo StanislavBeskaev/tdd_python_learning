9a223e1e391be2aae32d76568f8a375510175e12
Станислав Бескаев <stanislav.beskaev@gmail.com>
2023-08-08 15:03:16 +0300

    с 378, добавление сообщения проверки email и создание маркера

--- superlists/accounts/tests/test_views.py
+++ superlists/accounts/tests/test_views.py
@@ -2,23 +2,63 @@ from unittest.mock import patch, MagicMock
 
 from django.test import TestCase
 
+from accounts.models import Token
+
 
 class SendLoginEmailViewTest(TestCase):
     """Тесты представления, которое отправляет сообщение для входа в систему"""
 
+    def send_login_email_request(self, email: str):
+        return self.client.post("/accounts/send_login_email", data={"email": email})
+
     def test_redirect_to_home_page(self):
         """Тест: переадресуется на домашнюю страницу"""
-        response = self.client.post("/accounts/send_login_email", data={"email": "edith@example.com"})
+        response = self.send_login_email_request("edith@example.com")
         self.assertRedirects(response, "/")
 
     @patch("accounts.views.send_mail")
     def test_sends_mail_to_address_from_post(self, mock_send_mail: MagicMock):
         """Тест: отправляется сообщение на адрес из метода post"""
-        self.client.post("/accounts/send_login_email", data={"email": "edith@example.com"})
-
+        self.send_login_email_request("edith@example.com")
         self.assertTrue(mock_send_mail.called)
         args, kwargs = mock_send_mail.call_args
         self.assertEqual(kwargs["subject"], "Your login link for Superlists")
-        self.assertEqual(kwargs["message"], "body text tbc")
         self.assertEqual(kwargs["from_email"], "noreply@superlists")
         self.assertEqual(kwargs["recipient_list"], ["edith@example.com"])
+
+    def test_adds_success_message(self):
+        """Тест: добавляется сообщение об успехе"""
+        response = self.client.post("/accounts/send_login_email", data={"email": "edith@example.com"}, follow=True)
+
+        message = list(response.context["messages"])[0]
+        self.assertEqual(
+            message.message,
+            "Check your email, you'll find a message with a link that will log you into the site."
+        )
+        self.assertEqual(message.tags, "success")
+
+    def test_creates_token_associated_with_email(self):
+        """Тест: создаётся маркер, связанный с электронной почтой"""
+        self.send_login_email_request("edith@example.com")
+
+        token = Token.objects.first()
+        self.assertEqual(token.email, "edith@example.com")
+
+    @patch("accounts.views.send_mail")
+    def test_sends_link_to_login_using_token_uid(self, mock_send_mail: MagicMock):
+        """Тест: отсылается ссылка на вход в систему, используя uid маркера"""
+        self.send_login_email_request("edith@example.com")
+
+        token = Token.objects.first()
+        expected_url = f"http://testserver/accounts/login?token={token.uid}"
+        args, kwargs = mock_send_mail.call_args
+        self.assertIn(expected_url, kwargs["message"])
+
+
+class LoginViewTest(TestCase):
+    """Тесты представления входа в систему"""
+
+    def test_redirects_to_home_page(self):
+        """Тест: переадресуется на домашнюю страницу"""
+        response = self.client.get("/accounts/login?token=abcd123")
+        self.assertRedirects(response, "/")
--- superlists/accounts/views.py
+++ superlists/accounts/views.py
@@ -1,15 +1,31 @@
+from django.contrib import messages
 from django.core.mail import send_mail
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import redirect
+from django.urls import reverse
+
+from accounts.models import Token
 
 
 def send_login_email(request: HttpRequest) -> HttpResponse:
     """Отправка сообщения для входа в систему"""
     email = request.POST["email"]
+    token = Token.objects.create(email=email)
+    url = request.build_absolute_uri(reverse("login") + "?token=" + str(token.uid))
+    message_body = f"Use this link to log in:\n\n{url}"
     send_mail(
         subject="Your login link for Superlists",
-        message="body text tbc",
+        message=message_body,
         from_email="noreply@superlists",
         recipient_list=[email]
     )
+    messages.success(
+        request,
+        "Check your email, you'll find a message with a link that will log you into the site."
+    )
+    return redirect("/")
+
+
+def login(request: HttpRequest) -> HttpResponse:
+    """Регистрация в системе"""
     return redirect("/")
