874d331a8bc3a723bcbcdfa7451e9cb8b5100584
Станислав Бескаев <stanislav.beskaev@gmail.com>
2023-08-08 11:42:55 +0300

    c 358, у пользователя email теперь первичный ключ

--- superlists/accounts/models.py
+++ superlists/accounts/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class User(models.Model):
     """Пользователь"""
-    email = models.EmailField(unique=True)
+    email = models.EmailField(primary_key=True)
     REQUIRED_FIELDS = []
     USERNAME_FIELD = 'email'
     is_anonymous = False
--- superlists/accounts/tests/test_models.py
+++ superlists/accounts/tests/test_models.py
@@ -11,3 +11,8 @@ class UserModelTest(TestCase):
         """Тест: пользователя можно создать только с email"""
         user = User(email='a@b.com')
         user.full_clean()
+
+    def test_email_is_primary_key(self):
+        """Тест: адрес электронной почты является первичным ключом"""
+        user = User(email='a@b.com')
+        self.assertEqual(user.pk, 'a@b.com')
