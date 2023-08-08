1e54bf677434d6a058b2153d7fbbc0fae8e59e44
Станислав Бескаев <stanislav.beskaev@gmail.com>
2023-08-08 12:32:02 +0300

    c 371, использование patch для перехвата отправки писем

--- superlists/accounts/tests/test_views.py
+++ superlists/accounts/tests/test_views.py
@@ -1,7 +1,6 @@
-import accounts.views
-from django.test import TestCase
+from unittest.mock import patch, MagicMock
 
-from accounts import views
+from django.test import TestCase
 
 
 class SendLoginEmailViewTest(TestCase):
@@ -12,22 +11,14 @@ class SendLoginEmailViewTest(TestCase):
         response = self.client.post("/accounts/send_login_email", data={"email": "edith@example.com"})
         self.assertRedirects(response, "/")
 
-    def test_sends_mail_to_address_from_post(self):
+    @patch("accounts.views.send_mail")
+    def test_sends_mail_to_address_from_post(self, mock_send_mail: MagicMock):
         """Тест: отправляется сообщение на адрес из метода post"""
-        self.send_mail_called = False
+        self.client.post("/accounts/send_login_email", data={"email": "edith@example.com"})
 
-        def fake_send_mail(subject, message, from_email, recipient_list):
-            """Поддельная функция send_mail"""
-            self.send_mail_called = True
-            self.subject = subject
-            self.message = message
-            self.from_email = from_email
-            self.to_list = recipient_list
-
-        accounts.views.send_mail = fake_send_mail
-
-        response = self.client.post("/accounts/send_login_email", data={"email": "edith@example.com"})
-        self.assertTrue(self.send_mail_called)
-        self.assertEqual(self.subject, "Your login link for Superlists")
-        self.assertEqual(self.from_email, "noreply@superlists")
-        self.assertEqual(self.to_list, ["edith@example.com"])
+        self.assertTrue(mock_send_mail.called)
+        args, kwargs = mock_send_mail.call_args
+        self.assertEqual(kwargs["subject"], "Your login link for Superlists")
+        self.assertEqual(kwargs["message"], "body text tbc")
+        self.assertEqual(kwargs["from_email"], "noreply@superlists")
+        self.assertEqual(kwargs["recipient_list"], ["edith@example.com"])
