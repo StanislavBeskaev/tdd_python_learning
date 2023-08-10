c1dacff669f1791ecaf4fa6edca6d7dc67a84285
Станислав Бескаев <stanislav.beskaev@gmail.com>
2023-08-10 09:06:06 +0300

    с 408, декоратор wait для явного ожидания, применение для всех ожидающих мест

--- superlists/functional_tests/base.py
+++ superlists/functional_tests/base.py
@@ -13,6 +13,20 @@ MAX_WAIT = 2
 WAIT_TIMEOUT = 0.2
 
 
+def wait(fn: Callable) -> Callable:
+    def modified_fn(*args, **kwargs):
+        start_time = time.monotonic()
+        while True:
+            try:
+                return fn(*args, **kwargs)
+            except (AssertionError, WebDriverException) as e:
+                if time.monotonic() - start_time > MAX_WAIT:
+                    raise e
+                time.sleep(WAIT_TIMEOUT)
+
+    return modified_fn
+
+
 class FunctionalTest(StaticLiveServerTestCase):
     """Функциональный тест"""
 
@@ -34,49 +48,33 @@ class FunctionalTest(StaticLiveServerTestCase):
         inputbox.send_keys(text)
         inputbox.send_keys(Keys.ENTER)
 
+    @wait
     def wait_for_row_in_list_table(self, row_text: str):
         """Ожидать строку в таблице списка"""
-        start_time = time.monotonic()
-        while True:
-            try:
-                table = self.browser.find_element(by=By.ID, value="id_list_table")
-                rows = table.find_elements(by=By.TAG_NAME, value="tr")
-                self.assertIn(row_text, [row.text for row in rows])
-                return
-            except (AssertionError, WebDriverException) as e:
-                if time.monotonic() - start_time > MAX_WAIT:
-                    raise e
-                time.sleep(WAIT_TIMEOUT)
+        table = self.browser.find_element(by=By.ID, value="id_list_table")
+        rows = table.find_elements(by=By.TAG_NAME, value="tr")
+        self.assertIn(row_text, [row.text for row in rows])
 
     def find_inputbox(self) -> WebElement:
         """Найти поле ввода элемента"""
         inputbox = self.browser.find_element(by=By.ID, value="id_text")
         return inputbox
 
-    @staticmethod
-    def wait_for(fn: Callable):
+    @wait
+    def wait_for(self, fn: Callable):
         """Ожидать"""
-        start_time = time.monotonic()
-        while True:
-            try:
-                return fn()
-            except (AssertionError, WebDriverException) as e:
-                if time.monotonic() - start_time > MAX_WAIT:
-                    raise e
-                time.sleep(WAIT_TIMEOUT)
+        return fn()
 
+    @wait
     def wait_to_be_logged_in(self, email: str):
         """Ожидать входа в систему"""
-        self.wait_for(
-            lambda: self.browser.find_element(by=By.LINK_TEXT, value="Log out")
-        )
+        self.browser.find_element(by=By.LINK_TEXT, value="Log out")
         navbar = self.browser.find_element(by=By.CSS_SELECTOR, value=".navbar")
         self.assertIn(email, navbar.text)
 
+    @wait
     def wait_to_be_logged_out(self, email: str):
         """Ожидать выхода из системы"""
-        self.wait_for(
-            lambda: self.browser.find_element(by=By.NAME, value="email")
-        )
+        self.browser.find_element(by=By.NAME, value="email")
         navbar = self.browser.find_element(by=By.CSS_SELECTOR, value=".navbar")
         self.assertNotIn(email, navbar.text)
