b6e361d5f90cf755e32363bb8de5ec028c4815bd
Станислав Бескаев <beskaev@skbkontur.ru>
2023-08-23 16:58:24 +0300

    с 573, тестирование ajax с помощью sinonjs, тесты

--- superlists/lists/static/list.js
+++ superlists/lists/static/list.js
@@ -1,6 +1,17 @@
 window.Superlists = {};
-window.Superlists.initialize = function () {
+window.Superlists.initialize = function (url) {
   $('input[name="text"]').on('keypress', function () {
     $('.has-error').hide();
   });
+  if (url) {
+    $.get(url).done(function (response) {
+      let rows = ''
+      for (let i=0; i<response.length; i++) {
+        const item = response[i]
+        rows += '\n<tr><td>' + (i+1) + ': ' + item.text + '</td></tr>>'
+      }
+      $('#id_list_table').html(rows)
+    })
+  }
+
 };
